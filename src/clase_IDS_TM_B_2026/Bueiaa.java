286e72d2ca3d42fab89100e7e4b9fc83abc69bce
Ricardo <jeal_24@alu.uabcs.mx>
2026-02-20 10:58:01 -0700

    Pre diseño Log in

--- src/clase_IDS_TM_B_2026/Bueiaa.java
+++ src/clase_IDS_TM_B_2026/Bueiaa.java
@@ -10,17 +10,8 @@ public class Bueiaa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		JFrame ventana = new JFrame();
-		
-		ventana.setVisible(true);//VER VENTANA
-		ventana.setSize(500,500);//TAMAÑO VENTANA
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//DETENER PROGRAMA AL CERRAR
-		ventana.setLocationRelativeTo(null);//PONER EN CENTRO
-		ventana.setMinimumSize(new Dimension(500,500));//TAMAÑO MINIMO 
-		ventana.setMaximumSize(new Dimension(800,800));//TAMAÑO MAXIMO
-		ventana.setTitle("Hola");//TITULO DE LA VENTANA
-		ventana.setBackground(Color.black);
-		ventana.setOpacity(100);
+		 
+
 		
 		
 	}
--- src/clase_IDS_TM_B_2026/Ventana.java
+++ src/clase_IDS_TM_B_2026/Ventana.java
@@ -1,9 +1,63 @@
 package clase_IDS_TM_B_2026;
 
-public class Ventana {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Ventana extends JFrame{
 
 	public Ventana() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub.
+		this.setVisible(true);//VER VENTANA
+		this.setSize(500,500);//TAMAÑO VENTANA
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//DETENER PROGRAMA AL CERRAR
+		this.setLocationRelativeTo(null);//PONER EN CENTRO
+		this.setMinimumSize(new Dimension(500,500));//TAMAÑO MINIMO 
+		this.setMaximumSize(new Dimension(800,800));//TAMAÑO MAXIMO
+		this.setTitle("Hola");//TITULO DE LA VENTANA
+		this.setBackground(Color.black);
+		this.setOpacity(100);
+		
+		JPanel contenedor = new JPanel();
+		contenedor.setOpaque(true);
+		contenedor.setBackground(Color.black);
+		contenedor.setSize(500,500);
+		contenedor.setLocation(0, 0);
+		contenedor.setLayout(null);
+		this.add(contenedor);
+		
+		JLabel title_login = new JLabel();
+		title_login.setText("Bienvenido");
+		title_login.setSize(200, 30);
+		title_login.setOpaque(true);
+		title_login.setLocation(150, 10);
+		title_login.setBackground(Color.white);
+		title_login.setFont(new Font("Arial",Font.BOLD,22));
+		title_login.setHorizontalAlignment(JLabel.CENTER);
+		contenedor.add(title_login);
+		
+		JTextField username = new JTextField();
+		username.setSize(300,30);
+		username.setLocation(30, 150);
+		username.setFont(new Font("Arial",Font.BOLD,22));
+		contenedor.add(username);
+		
+		JButton acceder = new JButton();
+		acceder.setText("Acceder");
+		acceder.setLocation(200, 400);
+		acceder.setSize(120,50);
+		acceder.setFont(new Font("Arial",Font.BOLD,22));
+		contenedor.add(acceder);
+		
+		contenedor.repaint();
+		contenedor.revalidate();
+		
 	}
 
 }
